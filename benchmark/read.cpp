67751ed75b302c68d4d166f1972d69dc337edff0
Andrian Rabysh <284603@niuitmo.ru>
2024-04-14 04:16:47 +0300

    Add benchmarks and improve hash

--- benchmark/read.cpp
+++ benchmark/read.cpp
@@ -5,49 +5,113 @@
 
 using components::document::document_t;
 
-static void document_read(benchmark::State& state) {
-  state.PauseTiming();
+void read_wrong(benchmark::State &state) {
   auto allocator = std::pmr::unsynchronized_pool_resource();
   auto doc = gen_doc(1000, &allocator);
-  std::string key_int{"/count"};
-  std::string key_str{"/countStr"};
-  std::string key_double{"/countDouble"};
-  std::string key_bool{"/countBool"};
-  std::string key_array{"/countArray"};
-  std::string key_dict{"/countDict"};
+  std::string_view key_bool{"/countBool"};
 
-  auto f = [&doc](const std::string& key) {
-    doc->is_exists(key);
-    doc->is_null(key);
-    doc->is_bool(key);
-    doc->is_ulong(key);
-    doc->is_long(key);
-    doc->is_double(key);
-    doc->is_string(key);
-    doc->is_array(key);
-    doc->is_dict(key);
+  auto f = [&doc, key_bool]() {
+    doc->is_long(key_bool);
+    doc->is_ulong(key_bool);
+    doc->is_double(key_bool);
 
-    doc->get_bool(key);
-    doc->get_ulong(key);
-    doc->get_long(key);
-    doc->get_double(key);
-    doc->get_string(key);
-    doc->get_array(key);
-    doc->get_dict(key);
+    doc->get_ulong(key_bool);
+    doc->get_long(key_bool);
+    doc->get_double(key_bool);
+    doc->get_string(key_bool);
+    doc->get_array(key_bool);
+    doc->get_dict(key_bool);
   };
 
-  state.ResumeTiming();
-  for (auto _ : state) {
+  for (auto _: state) {
     for (int i = 0; i < state.range(0); ++i) {
-      f(key_int);
-      f(key_str);
-      f(key_double);
-      f(key_bool);
-      f(key_array);
-      f(key_dict);
+      f();
     }
   }
 }
-BENCHMARK(document_read)->Arg(10000);
+BENCHMARK(read_wrong)->Arg(100000);
+
+void read(benchmark::State &state) {
+  auto allocator = std::pmr::unsynchronized_pool_resource();
+  auto doc = gen_doc(1000, &allocator);
+  std::string_view key_int{"/count"};
+  std::string_view key_str{"/countStr"};
+  std::string_view key_double{"/countDouble"};
+  std::string_view key_bool{"/countBool"};
+  std::string_view key_array{"/countArray"};
+  std::string_view key_dict{"/countDict"};
+
+  auto f = [
+          &doc,
+          key_int,
+          key_str,
+          key_double,
+          key_bool,
+          key_array,
+          key_dict
+          ]() {
+    doc->is_exists(key_int);
+    doc->is_long(key_int);
+    doc->is_ulong(key_int);
+    doc->is_double(key_double);
+
+    doc->get_bool(key_bool);
+    doc->get_long(key_int);
+    doc->get_ulong(key_int);
+    doc->get_double(key_double);
+    doc->get_string(key_str);
+    doc->get_array(key_array);
+    doc->get_dict(key_dict);
+  };
+
+  for (auto _: state) {
+    for (int i = 0; i < state.range(0); ++i) {
+      f();
+    }
+  }
+}
+BENCHMARK(read)->Arg(100000);
+
+void deep_read(benchmark::State &state) {
+  auto allocator = std::pmr::unsynchronized_pool_resource();
+  auto doc = gen_doc(1000, &allocator);
+  std::string_view array_int_key{"/countArray/3"};
+  std::string_view array_array_key{"/nestedArray/2"};
+  std::string_view dict_dict_key{"/mixedDict/1001"};
+  std::string_view array_dict_key{"/dictArray/3"};
+  std::string_view array_array_int_key{"/nestedArray/2/2"};
+  std::string_view array_dict_int_key{"/dictArray/3/number"};
+  std::string_view dict_dict_bool_key{"/mixedDict/1001/odd"};
+
+  auto f = [
+          &doc,
+          array_int_key,
+          array_array_key,
+          dict_dict_key,
+          array_dict_key,
+          array_array_int_key,
+          array_dict_int_key,
+          dict_dict_bool_key
+  ]() {
+    doc->is_exists(array_int_key);
+    doc->is_int(array_int_key);
+    doc->is_long(array_int_key);
+
+    doc->get_bool(dict_dict_bool_key);
+    doc->get_long(array_int_key);
+    doc->get_long(array_array_int_key);
+    doc->get_long(array_dict_int_key);
+    doc->get_array(array_array_key);
+    doc->get_dict(dict_dict_key);
+    doc->get_dict(array_dict_key);
+  };
+
+  for (auto _: state) {
+    for (int i = 0; i < state.range(0); ++i) {
+      f();
+    }
+  }
+}
+BENCHMARK(deep_read)->Arg(100000);
 
 BENCHMARK_MAIN();
